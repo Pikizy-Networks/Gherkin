03b524c19b0d277f17ab9f1cd0d8e050c1567da3
Luccas Cabrini <85950233+luccascabrini@users.noreply.github.com>
2021-12-03 21:16:56 -0300

    Update parte1-qa.py

--- parte1-qa.py
+++ parte1-qa.py
@@ -5,6 +5,8 @@ Dado que navego para a página de busca do banco de questões
 E digito 'Science: Computers' no campo de busca
 Quando clico no botão de buscar
 Então visualizo uma mensagem de erro com o texto 'No questions found.'
+
+.
 """
 
 from selenium.webdriver import Firefox
